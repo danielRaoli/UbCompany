2ec58ccc24ba1de983ca57d1ef5b4e8b898b8e96
danielRaoli <danielraoli074@gmail.com>
2024-09-30 20:31:48 -0300

    alteração no footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -21,7 +21,7 @@ const message = ref('')
 
 <template>
     <v-footer class="d-flex flex-column w-100 pa-4 bg-indigo-darken-4">
-        <div class="d-flex flex-column flex-md-row ga-4 pa-md-16  pa-2 align-center justify-space-around w-100">
+        <div class="d-flex flex-column flex-md-row ga-4 pa-md-16  pa-2 align-center justify-space-around w-100 my-10">
 
             <v-sheet class="d-flex flex-column flex-1 bg-indigo-darken-4" max-width="450px">
                 <span class="text-center font-weight-bold text-primary text-h4 mb-10">Entre em contato e coloque os
@@ -44,9 +44,9 @@ const message = ref('')
 
             </v-sheet>
 
-            <div class="d-flex flex-column ga-md-8 ga-2 align-center h-100 justify-center flex-1">
-                <span class="text-primary">Social</span>
-                <div class="d-flex flex-md-column w-100 ga-2 align-md-center">
+            <div class="d-flex ga-4 ga-md-8 flex-column align-md-center h-100 justify-md-center  flex-1">
+                <span class="text-primary text-center">Social</span>
+                <div class="d-flex flex-md-column w-100 ga-2 ga-md-8 align-md-center justify-md-center h-auto">
                     <a v-for="link in links" :key="icon" :icon="icon" :href="link.link" class="text-decoration-none"
                         color="primary" target="_blank">
                         <v-icon color="primary">{{ link.icon }}</v-icon>
@@ -57,12 +57,16 @@ const message = ref('')
 
 
             </div>
-            <div class="d-flex ga-md-8 ga-4 flex-column align-center h-100 justify-center flex-1">
-                <span class="text-primary">Menu</span>
 
-                <a href="#servicos" class=" text-primary text-decoration-none">Serviços</a>
-                <a href="#projetos" class=" text-primary text-decoration-none">Projetos</a>
-                <a href="#beneficios" class=" text-primary text-decoration-none">Benefícios</a>
+            <div class="d-flex ga-md-8 ga-4 flex-column align-md-center h-100 justify-md-center  flex-1">
+
+                <span class="text-primary text-center">Menu</span>
+                <div class="d-flex flex-md-column ga-2 ga-md-8 align-md-center">
+                    <a href="#servicos" class=" text-primary text-decoration-none">Serviços</a>
+                    <a href="#projetos" class=" text-primary text-decoration-none">Projetos</a>
+                    <a href="#beneficios" class=" text-primary text-decoration-none">Benefícios</a>
+                </div>
+
             </div>
 
         </div>
